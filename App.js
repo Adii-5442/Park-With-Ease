0b39ea76e31a184eca9c028ca5527fe24e569b7d
Aditya <adi.sh5442@gmail.com>
2023-01-01 23:57:40 +0530

    Welcome Page created

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import { StackActions } from '@react-navigation/native';
 import {NavigationContainer} from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Welcome from './screens/Welcome';
 
 
 const Stack = createNativeStackNavigator();
@@ -11,8 +12,8 @@ const App = () => {
 
   const Home = () => {
     return(
-      <View>
-        <Text style = {{fontSize : 90 , color  : 'black'}}>
+      <View style = {{backgroundColor:'black',flex:1}}>
+        <Text style = {{fontSize : 90 , color  : 'white'}}>
           HELLO   DARKFIRE
         </Text>
       </View>
@@ -24,6 +25,7 @@ const App = () => {
   return(
     <NavigationContainer>
       <Stack.Navigator>
+        <Stack.Screen name = "Welcome" component={Welcome} options = {{headerShown:false}}/>
         <Stack.Screen name = "Home" component={Home}/>
       </Stack.Navigator>
 
---
+++ screens/Welcome.js
@@ -0,0 +1,42 @@
+import { Text, View } from 'react-native'
+import React, { Component } from 'react'
+import { Image } from 'react-native'
+import * as Progress from 'react-native-progress';
+import { useNavigation } from '@react-navigation/native';
+const Welcome = () => {
+    const navigation = useNavigation();
+    setTimeout(function(){
+        navigation.navigate('Home')
+    },3000)
+
+
+    return (
+        <View style = {{flex:1,backgroundColor:'#000000'}}>
+            <View style = {{flex:0.8}}>
+                <Image 
+                    source = {require('../assets/images/ParkWelcLogo.jpeg')} 
+                    style = {{flex:1 , width:'100%', height:'120%' , resizeMode :'contain'}}
+                />
+            </View>
+            <View>
+                <Progress.Bar 
+                    animationType={"spring"} 
+                    useNativeDriver={true} 
+                    indeterminate = {true}
+                    progress={0.4} 
+                    width={null} 
+                    animated ={true}
+                    color = {"#FFFFFF"}
+                    style = {{marginLeft:50, marginRight:50}} 
+                />
+            </View>
+
+        </View>
+
+    )
+
+
+
+}
+
+export default Welcome
